0d367b622981b551bb5bc3125a0009c429a70dab
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-06-18 00:22:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello World from Python Flask!'
+    return 'Hello World from Python Flask!..This is development cluster'
 
 app.run(host='0.0.0.0', port=5000)
